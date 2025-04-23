88be4d496645a8b2d4fc27831a9501e1d654f7a1
Gilgalad195 <gilgalad195@gmail.com>
2025-04-23 08:09:32 -0500

    grouped draw and update objects

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     
     while True:
@@ -18,7 +22,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
